c1c92e5e18c292fa85dcc25ddad47b9462a51622
adreno255 <angelomarkflores125@gmail.com>
2024-05-22 17:40:01 +0800

    added category course buttons

--- script.js
+++ script.js
@@ -40,9 +40,87 @@ document.addEventListener("DOMContentLoaded", function() {
         loadHTMLFile(page + '.html', 'dynamic-content', function() {
             if(page == 'courses'){
                 loadPage('agriculture', 'Courses HTML/Agriculture/agriculture');
+                loadPage('architecture-and-design', 'Courses HTML/Architecture-and-Design/architecture-and-design');
+                loadPage('business', 'Courses HTML/Business/business');
+                loadPage('education', 'Courses HTML/Education/education');
+                loadPage('engineering', 'Courses HTML/Engineering/engineering');
+                loadPage('formal-sciences', 'Courses HTML/Formal-Sciences/formal-sciences');
+                loadPage('health-sciences', 'Courses HTML/Health-Sciences/health-sciences');
+                loadPage('humanities', 'Courses HTML/Humanities/humanities');
+                loadPage('media-and-communication', 'Courses HTML/Media-and-Communication/media-and-communication');
+                loadPage('natural-sciences', 'Courses HTML/Natural-Sciences/natural-sciences');
+                loadPage('nutrition', 'Courses HTML/Nutrition/nutrition');
+                loadPage('public-administration', 'Courses HTML/Public-Administration/public-administration');
+                loadPage('social-sciences', 'Courses HTML/Social-Sciences/social-sciences');
+                loadPage('transportation', 'Courses HTML/Transportation/transportation');
             }
 
             if(page == 'Courses HTML/Agriculture/agriculture'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Architecture-and-Design/architecture-and-design'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Business/business'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Education/education'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Engineering/engineering'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Formal-Sciences/formal-sciences'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Health-Sciences/health-sciences'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Humanities/humanities'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Media-and-Communication/media-and-communication'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Natural-Sciences/natural-sciences'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Nutrition/nutrition'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Public-Administration/public-administration'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Social-Sciences/social-sciences'){
+                loadPage('back-button', 'courses');
+                loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
+            }
+
+            if(page == 'Courses HTML/Transportation/transportation'){
                 loadPage('back-button', 'courses');
                 loadPage('bs-agriculture', 'Courses HTML/Agriculture/bs-agriculture');
             }
